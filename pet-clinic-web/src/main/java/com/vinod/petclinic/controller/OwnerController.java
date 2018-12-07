ba686a96b4a11fba6beb0704478b745a2c3d1c58
vihi1215 <optus2018@123>
2018-12-07 12:13:06 +0530

    Implemented code to display Owners on Lsit Page. Closes #12.

--- pet-clinic-web/src/main/java/com/vinod/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/vinod/petclinic/controller/OwnerController.java
@@ -1,14 +1,28 @@
 package com.vinod.petclinic.controller;
 
+import com.vinod.petclinic.model.Owner;
+import com.vinod.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
+    private OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"","/","/index","/index.html"})
-    public  String list(){
+    public  String list(Model model){
+        Set<Owner> owners = ownerService.findAll();
+        model.addAttribute("owners",owners);
+        System.out.println("Adding owners to model : "+owners);
         return "owner/index";
     }
 }
